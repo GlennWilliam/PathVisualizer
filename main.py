43c68314bd2e7dc4fcd46de5c4cc45f15dca67ca
GlennWilliam <glennwilliam1009@gmail.com>
2024-01-23 00:38:59 -0800

    Fix bug

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def algorithm(draw, grid, start, end):
     # To change the algorithm, uncomment the algorithm you wnat to use, and comment the previous one. 
     # Make sure there is only one algorithm that is running
 
-    # dijkstra(draw, grid, start, end)
+    dijkstra(draw, grid, start, end)
     # aStar(draw, grid, start, end)
-    dfs(draw, grid, start, end)
+    # dfs(draw, grid, start, end)
     # bfs(draw, grid, start, end)
     # greedyBestFirstSearch(draw, grid, start, end)
     
